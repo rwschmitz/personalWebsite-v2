507f4571a6d6ab4532ae826d5172e456a8638dd7
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-28 21:53:04 -0500

    updating home section

--- src/js/home.js
+++ src/js/home.js
@@ -1,25 +1,11 @@
-/*var hobby1 = document.getElementById("hiking");
-var hobby2 = document.getElementById("cooking");
-var hobby3 = document.getElementById("gaming");
-var hobby4 = document.getElementById("engineering");
-var hobby5 = document.getElementById("programming");
-
-function hideHobby1 () {
-    hobby1.style.display = 'none';
-}
-
-hobby1.addEventListener('click', hideHobby1, false);*/
-
-// spacer
-
 var hobbies = document.getElementsByClassName("icons");
 
 hobbies[0].onclick = function () {
     for(var i = 0; i < 5; i++) {
         if(i===0) {
-            hobbies[0].style.display = 'none';
+            hobbies[0].style.display = 'block';
         } else {
-            hobbies[i].style.display = 'block';
+            hobbies[i].style.display = 'none';
         }
     }
 };
@@ -27,9 +13,9 @@ hobbies[0].onclick = function () {
 hobbies[1].onclick = function () {
     for(var i = 0; i < 5; i++) {
         if(i===1) {
-            hobbies[1].style.display = 'none';
+            hobbies[1].style.display = 'block';
         } else {
-            hobbies[i].style.display = 'block';
+            hobbies[i].style.display = 'none';
         }
     }
 };
@@ -37,9 +23,9 @@ hobbies[1].onclick = function () {
 hobbies[2].onclick = function () {
     for(var i = 0; i < 5; i++) {
         if(i===2) {
-            hobbies[2].style.display = 'none';
+            hobbies[2].style.display = 'block';
         } else {
-            hobbies[i].style.display = 'block';
+            hobbies[i].style.display = 'none';
         }
     }
 };
@@ -47,9 +33,9 @@ hobbies[2].onclick = function () {
 hobbies[3].onclick = function () {
     for(var i = 0; i < 5; i++) {
         if(i===3) {
-            hobbies[3].style.display = 'none';
+            hobbies[3].style.display = 'block';
         } else {
-            hobbies[i].style.display = 'block';
+            hobbies[i].style.display = 'none';
         }
     }
 };
@@ -57,9 +43,9 @@ hobbies[3].onclick = function () {
 hobbies[4].onclick = function () {
     for(var i = 0; i < 5; i++) {
         if(i===4) {
-            hobbies[4].style.display = 'none';
+            hobbies[4].style.display = 'block';
         } else {
-            hobbies[i].style.display = 'block';
+            hobbies[i].style.display = 'none';
         }
     }
 };
