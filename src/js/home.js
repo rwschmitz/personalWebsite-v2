3090fc06ee94b9328364d56dbe30836125880860
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-28 20:44:30 -0500

    updating home section

--- src/js/home.js
+++ src/js/home.js
@@ -17,7 +17,21 @@ var hobbies = document.querySelectorAll('.icons');
 
 function hideHobbies () {
     for (var i = 0; i < 5; i++) {
-        console.log(hobbies[i]);
+        if(i === 0) {
+        console.log(hobbies[0]);
+        }
+        if(i === 1) {
+        console.log(hobbies[1]);
+        }
+        if(i === 2) {
+        console.log(hobbies[2]);
+        }
+        if(i === 3) {
+        console.log(hobbies[3]);
+        }
+        if(i === 4) {
+        console.log(hobbies[4]);
+        }
     }
 }
 
