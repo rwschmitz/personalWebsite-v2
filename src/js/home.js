1a0dd945616aabb52babd2cea68b28fab5db370d
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-28 19:38:47 -0500

    updating home section

--- src/js/home.js
+++ src/js/home.js
@@ -1,4 +1,4 @@
-var hobby1 = document.getElementById("hiking");
+/*var hobby1 = document.getElementById("hiking");
 var hobby2 = document.getElementById("cooking");
 var hobby3 = document.getElementById("gaming");
 var hobby4 = document.getElementById("engineering");
@@ -8,4 +8,17 @@ function hideHobby1 () {
     hobby1.style.display = 'none';
 }
 
-hobby1.addEventListener('click', hideHobby1, false);
+hobby1.addEventListener('click', hideHobby1, false);*/
+
+// spacer
+
+var hobbies = document.querySelectorAll('.icons');
+
+function hideHobbies () {
+    for(i = 0; i < 6; i++) {
+        hobbies[0].style.display = 'none';
+    }
+
+}
+
+hobbies.addEventListener('click', hideHobbies, false);
