7e0999b80228583fe5787f59c9eeabb494273834
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-28 20:50:53 -0500

    updating home section

--- src/js/home.js
+++ src/js/home.js
@@ -16,28 +16,26 @@ var hobbies = document.getElementsByClassName("icons");
 
 
 function hideHobbies () {
-    for (var i = 0; i < 5; i++) {
-        if(i === 0) {
+        if(hobbies[0]) {
         console.log(hobbies[0]);
-        break;
+
         }
         if(i === 1) {
         console.log(hobbies[1]);
-        break;
+
         }
         if(i === 2) {
         console.log(hobbies[2]);
-        break;
+
         }
         if(i === 3) {
         console.log(hobbies[3]);
-        break;
+ 
         }
         if(i === 4) {
         console.log(hobbies[4]);
-        break;
+   
         }
-    }
 }
 
 hobbies[0].addEventListener('click', hideHobbies, false);
