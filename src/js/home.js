008abc9edbfb1d5f895a4f98790616a3dae85cf3
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-29 23:51:37 -0500

    testing home.js

--- src/js/home.js
+++ src/js/home.js
@@ -19,6 +19,74 @@ hobbies[0].onclick = function () {
     }
 };
 
+hobbies[1].onclick = function () {
+    if (!active) {
+    hobbies[0].style.display = 'none';
+    hobbies[2].style.display = 'none';
+    hobbies[3].style.display = 'none';
+    hobbies[4].style.display = 'none';
+    active = true;
+    }
+    else {
+    hobbies[0].style.display = 'block';
+    hobbies[2].style.display = 'block';
+    hobbies[3].style.display = 'block';
+    hobbies[4].style.display = 'block';
+    active = false;
+    }
+};
+
+hobbies[2].onclick = function () {
+    if (!active) {
+    hobbies[0].style.display = 'none';
+    hobbies[1].style.display = 'none';
+    hobbies[3].style.display = 'none';
+    hobbies[4].style.display = 'none';
+    active = true;
+    }
+    else {
+    hobbies[0].style.display = 'block';
+    hobbies[1].style.display = 'block';
+    hobbies[3].style.display = 'block';
+    hobbies[4].style.display = 'block';
+    active = false;
+    }
+};
+
+hobbies[3].onclick = function () {
+    if (!active) {
+    hobbies[0].style.display = 'none';
+    hobbies[1].style.display = 'none';
+    hobbies[2].style.display = 'none';
+    hobbies[4].style.display = 'none';
+    active = true;
+    }
+    else {
+    hobbies[0].style.display = 'block';
+    hobbies[1].style.display = 'block';
+    hobbies[2].style.display = 'block';
+    hobbies[4].style.display = 'block';
+    active = false;
+    }
+};
+
+hobbies[4].onclick = function () {
+    if (!active) {
+    hobbies[0].style.display = 'none';
+    hobbies[1].style.display = 'none';
+    hobbies[2].style.display = 'none';
+    hobbies[3].style.display = 'none';
+    active = true;
+    }
+    else {
+    hobbies[0].style.display = 'block';
+    hobbies[1].style.display = 'block';
+    hobbies[2].style.display = 'block';
+    hobbies[3].style.display = 'block';
+    active = false;
+    }
+};
+
 /*
 hobbies[0].onclick = function () {
     for(var i = 0; i < 5; i++) {
