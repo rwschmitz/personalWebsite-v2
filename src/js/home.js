07c4fcc3f579c3cd1c279a716b707ac7f9b8bec6
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-30 14:30:20 -0500

    updating home.js

--- src/js/home.js
+++ src/js/home.js
@@ -1,15 +1,7 @@
 var hobbies = document.getElementsByClassName("hobbies");
 var active = false;
 
-for (var i = 0; i < 5; i++) {
-    hobbies[i].onclick = function() {
-            hobbies[0].classList.add('hobbies-slide-left');
-            hobbies[1].classList.add('hobbies-slide-right');
-            hobbies[2].classList.add('hobbies-slide-left');
-            hobbies[3].classList.add('hobbies-slide-right');
-            hobbies[4].classList.add('hobbies-slide-left');
-    };
-}
+// For mouse input
 
 for (var i = 0; i < 5; i++) {
     hobbies[i].onmouseover = function() {
@@ -21,29 +13,14 @@ for (var i = 0; i < 5; i++) {
     };
 }
 
+// For click input
 
-/*
-hobbies[1].onclick = function() {
-    if (!active) {
-       for (i = 0; i < 1; i++) {
-        hobbies[i].classList.add('hobbies-slide');
-        hobbies[i].classList.remove('hobbies-return');
-       }
-       for (j = 2; j < 5; j++) {
-        hobbies[j].classList.add('hobbies-slide');
-        hobbies[j].classList.remove('hobbies-return');
-       }
-       active = true;
-    } else {
-       for (i = 0; i < 1; i++) {
-        hobbies[i].classList.remove('hobbies-slide');
-        hobbies[i].classList.add('hobbies-return');
-       }
-       for (j = 2; j < 5; j++) {
-        hobbies[j].classList.remove('hobbies-slide');
-        hobbies[j].classList.add('hobbies-return');
-       }
-       active = false;
-    }
- };
- */
+for (var i = 0; i < 5; i++) {
+    hobbies[i].onclick = function() {
+            hobbies[0].classList.add('hobbies-slide-left');
+            hobbies[1].classList.add('hobbies-slide-right');
+            hobbies[2].classList.add('hobbies-slide-left');
+            hobbies[3].classList.add('hobbies-slide-right');
+            hobbies[4].classList.add('hobbies-slide-left');
+    };
+}
