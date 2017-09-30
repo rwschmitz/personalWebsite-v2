afa484c8c64a5b3f44cb82b5d1b846504f83858b
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-30 13:41:49 -0500

    updating home.js

--- src/js/home.js
+++ src/js/home.js
@@ -1,28 +1,7 @@
 var hobbies = document.getElementsByClassName("hobbies");
-var arrows = document.getElementsByClassName("arrows");
 
 for (let i = 0; i < 5; i++) {
     hobbies[i].onclick = function() {
         hobbies[i].classList.add('hobbies-slide');
     };
 }
-
-
-/*
-hobbies[0].onclick = function () {
-    for(i = 0; i < 5; i++) {
-        hobbies[i].classList.add('hobbies-slide');
-    }
-};
-
-
-
-arrowsVis = window.onload = function () {
-    for(i = 0; i < 5; i++) {
-        arrows[i].classList.remove('arrows-hidden');
-        arrows[i].classList.add('arrows-visible');
-    }
-
-};
-*/
-
