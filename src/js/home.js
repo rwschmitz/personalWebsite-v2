53736144b208fe58579620b8b522331a1f723054
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-29 23:58:45 -0500

    testing home.js

--- src/js/home.js
+++ src/js/home.js
@@ -19,18 +19,16 @@ hobbies[0].onclick = function () {
 
 hobbies[1].onclick = function () {
     if (!active) {
-    hobbies[0].style.display = 'none';
-    hobbies[2].style.display = 'none';
-    hobbies[3].style.display = 'none';
-    hobbies[4].style.display = 'none';
+        for(i=2;i<5;i++) {
+    hobbies[i].style.display = 'none';
     active = true;
+        }
     }
     else {
-    hobbies[0].style.display = 'block';
-    hobbies[2].style.display = 'block';
-    hobbies[3].style.display = 'block';
-    hobbies[4].style.display = 'block';
+        for(i=2;i<5;i++) {
+    hobbies[i].style.display = 'block';
     active = false;
+        }
     }
 };
 
