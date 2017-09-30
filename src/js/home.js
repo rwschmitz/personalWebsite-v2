19e21e3b6ca2a3209311c2ea9eb68d9e1daa37cd
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-30 14:10:55 -0500

    updating home.js

--- src/js/home.js
+++ src/js/home.js
@@ -22,18 +22,22 @@ hobbies[0].onclick = function() {
 hobbies[1].onclick = function() {
     if (!active) {
        for (i = 0; i < 1; i++) {
-          hobbies[i].style.display = 'none';
+        hobbies[i].classList.add('hobbies-slide');
+        hobbies[i].classList.remove('hobbies-return');
        }
        for (j = 2; j < 5; j++) {
-          hobbies[j].style.display = 'none';
+        hobbies[j].classList.add('hobbies-slide');
+        hobbies[j].classList.remove('hobbies-return');
        }
        active = true;
     } else {
        for (i = 0; i < 1; i++) {
-          hobbies[i].style.display = 'block';
+        hobbies[i].classList.remove('hobbies-slide');
+        hobbies[i].classList.add('hobbies-return');
        }
        for (j = 2; j < 5; j++) {
-          hobbies[j].style.display = 'block';
+        hobbies[j].classList.remove('hobbies-slide');
+        hobbies[j].classList.add('hobbies-return');
        }
        active = false;
     }
