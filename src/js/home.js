2baebe25f62d175f9964d815257972e376bf14f2
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-30 12:10:50 -0500

    updating home.js

--- src/js/home.js
+++ src/js/home.js
@@ -6,13 +6,24 @@ For example, hobbies[2].onclick hides and shows elements while excluding element
 
 */
 
-var hobbies = document.getElementsByClassName("icons");
-var active = false;
+var hobbies = document.getElementsByClassName("hobbies");
+var arrows = document.getElementsByClassName("arrows");
+
 
 window.onload = function slide() {
     for(i = 0; i < 5; i++) {
-        hobbies[i].classList.add('icons-slide');
+        hobbies[i].classList.add('hobbies-slide');
     }
 };
 
 window.addEventListener('DOMContentLoaded', slide, false);
+
+window.onload = function arrows () {
+    for(i = 0; i < 5; i++) {
+        arrows[i].classList.remove('arrows-hidden');
+        arrows[i].classList.remove('arrows-visible');
+    }
+
+};
+
+window.addEventListener('DOMContentLoaded', arrows, false);
