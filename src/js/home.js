7e8de7115503560e94dd7e2283974ffff1a739e8
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-30 13:34:54 -0500

    updating home.js

--- src/js/home.js
+++ src/js/home.js
@@ -5,7 +5,7 @@ for (let i = 0; i < 5; i++) {
     hobbies[i].onmouseover = function() {
         hobbies[i].classList.add('hobbies-slide');
     };
-    hobbies[i].onmouseout = function () {
+    hobbies[i].onclick = function () {
         hobbies[i].classList.remove('hobbies-slide');
     };
 }
