3220daf73d8e1471aa9f4fcfd219a51026dfda81
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-30 12:49:29 -0500

    updating home.js

--- src/js/home.js
+++ src/js/home.js
@@ -10,7 +10,7 @@ var hobbies = document.getElementsByClassName("hobbies");
 var arrows = document.getElementsByClassName("arrows");
 
 
-slideLeft = hobbies[i].onclick = function () {
+slideLeft = hobbies.onclick = function () {
     for(i = 0; i < 5; i++) {
         hobbies[i].classList.add('hobbies-slide');
     }
