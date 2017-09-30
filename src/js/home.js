41d8e241a5466b7061d2ffe605764e4ff2dc691d
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-30 13:45:22 -0500

    updating home.js

--- src/js/home.js
+++ src/js/home.js
@@ -1,7 +1,16 @@
 var hobbies = document.getElementsByClassName("hobbies");
+var active = false;
 
-for (let i = 0; i < 5; i++) {
-    hobbies[i].onclick = function() {
-        hobbies[i].classList.add('hobbies-slide');
-    };
+for (let i = 1; i < 5; i++) {
+    if(!active) {
+        hobbies[i].onclick = function() {
+            hobbies[i].classList.remove('hobbies-return');
+            hobbies[i].classList.add('hobbies-slide');
+        };
+    } else {
+        hobbies[i].onclick = function() {
+            hobbies[i].classList.remove('hobbies-slide');
+            hobbies[i].classList.add('hobbies-return');
+        };
+    }
 }
