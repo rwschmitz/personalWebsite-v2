ad1695cab4f24dc832df9b92d646a6593112c3f1
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-10-01 02:50:21 -0500

    updating home spacing

--- src/js/home.js
+++ src/js/home.js
@@ -4,34 +4,34 @@ var hobbiesDesc = document.getElementsByClassName("hobbies-desc");
 
 for (i = 0; i < 6; i++) {
     hobbies[i].onclick = function() {
-        hobbies[0].classList.add("test");
-        hobbiesDesc[0].classList.add("blah");
+        hobbies[0].classList.add("home-icons");
+        hobbiesDesc[0].classList.add("home-text");
     };
 }
 
 function nextIcon() {
-   hobbies[1].classList.add("test2");
-   hobbiesDesc[1].classList.add("blah2");
+   hobbies[1].classList.add("home-icons2");
+   hobbiesDesc[1].classList.add("home-text2");
 }
 
 function nextIcon2() {
-   hobbies[2].classList.add("test3");
-   hobbiesDesc[2].classList.add("blah3");
+   hobbies[2].classList.add("home-icons3");
+   hobbiesDesc[2].classList.add("home-text3");
 }
 
 function nextIcon3() {
-   hobbies[3].classList.add("test4");
-   hobbiesDesc[3].classList.add("blah4");
+   hobbies[3].classList.add("home-icons4");
+   hobbiesDesc[3].classList.add("home-text4");
 }
 
 function nextIcon4() {
-   hobbies[4].classList.add("test5");
-   hobbiesDesc[4].classList.add("blah5");
+   hobbies[4].classList.add("home-icons5");
+   hobbiesDesc[4].classList.add("home-text5");
 }
 
 function nextIcon5() {
-   hobbies[5].classList.add("test6");
-   hobbiesDesc[5].classList.add("blah6");
+   hobbies[5].classList.add("home-icons6");
+   hobbiesDesc[5].classList.add("home-text6");
 }
 
 
