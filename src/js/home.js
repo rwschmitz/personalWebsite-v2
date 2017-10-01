6d797a8e1c1c638231e6b314baecd96a9d4aa647
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-09-30 20:48:33 -0500

    adding img

--- src/js/home.js
+++ src/js/home.js
@@ -1,6 +1,7 @@
 var hobbies = Array.from(document.getElementsByClassName('hobbies'));
 var classes = ['hobbies-slide-down'];
-/*var classes = ['hobbies-slide-left', 'hobbies-slide-right'];*/
+
+// For mouseover OR click
 
 function addHobbyClass (hobby, index) {
   hobby.classList.add(classes[index % classes.length]);
@@ -11,50 +12,40 @@ function hobbyEventListener () {
 }
 
 hobbies.map(function (hobby) {
-  document.addEventListener('loadEvent', hobbyEventListener);
   hobby.addEventListener('mouseover', hobbyEventListener);
   hobby.addEventListener('click', hobbyEventListener);
 });
 
-function test() {
+// For scrolling
+
+var last_known_scroll_position = 0;
+var ticking = false;
+
+function scrollDown(scroll_pos) {
+ hobbies[0].classList.add('hobbies-slide-down');
+ hobbies[1].classList.add('hobbies-slide-down');
+ hobbies[2].classList.add('hobbies-slide-down');
+ hobbies[3].classList.add('hobbies-slide-down');
+ hobbies[4].classList.add('hobbies-slide-down');
+}
+
+window.addEventListener('scroll', function(e) {
+  last_known_scroll_position = window.scrollY;
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      scrollDown(last_known_scroll_position);
+      ticking = false;
+    });
+  }
+  ticking = true;
+});
+
+function animateText() {
     $(function() {
        $('.info-down').animate({opacity: 1});
     });
  }
  
 for (var i = 0; i < 5; i++) {
-    hobbies[i].addEventListener("transitionend", test, false);
+    hobbies[i].addEventListener("transitionend", animateText, false);
 }
-
-
-
-/* Old code
-
-/*var hobbies = document.getElementsByClassName("hobbies");
-var active = false;*/
-
-// For mouse input
-
-/*for (var i = 0; i < 5; i++) {
-    hobbies[i].onmouseover = function() {
-            hobbies[0].classList.add('hobbies-slide-left');
-            hobbies[1].classList.add('hobbies-slide-right');
-            hobbies[2].classList.add('hobbies-slide-left');
-            hobbies[3].classList.add('hobbies-slide-right');
-            hobbies[4].classList.add('hobbies-slide-left');
-    };
-}
-
-// For click input
-
-for (var i = 0; i < 5; i++) {
-    hobbies[i].onclick = function() {
-            hobbies[0].classList.add('hobbies-slide-left');
-            hobbies[1].classList.add('hobbies-slide-right');
-            hobbies[2].classList.add('hobbies-slide-left');
-            hobbies[3].classList.add('hobbies-slide-right');
-            hobbies[4].classList.add('hobbies-slide-left');
-    };
-}
-
-*/
