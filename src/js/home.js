d55be9cb39117d16e5115e3fe60b3a59c43d9095
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-10-01 15:48:47 -0500

    updating home.js

--- src/js/home.js
+++ src/js/home.js
@@ -1,52 +1,42 @@
-/* jshint ignore: start */
+
 var hobbies = document.getElementsByClassName("hobbies");
 var hobbiesDesc = document.getElementsByClassName("hobbies-desc");
 
-for (i = 0; i < 6; i++) {
-    hobbies[i].onclick = function() {
-        hobbies[0].classList.add("home-icons");
-        hobbiesDesc[0].classList.add("home-text");
-    };
-}
+var blah1 = function test (){
+    hobbies[0].classList.add("home-icons");
+    hobbiesDesc[0].classList.add("home-text");
+};
 
-for (i = 0; i < 6; i++) {
-    hobbies[i].onmouseover = function() {
-        hobbies[0].classList.add("home-icons");
-        hobbiesDesc[0].classList.add("home-text");
-    };
-}
+var blah2 = function test (){
+    hobbies[1].classList.add("home-icons");
+    hobbiesDesc[1].classList.add("home-text");
+};
 
-function nextIcon() {
-   hobbies[1].classList.add("home-icons2");
-   hobbiesDesc[1].classList.add("home-text2");
-}
+var blah3 = function test (){
+    hobbies[2].classList.add("home-icons");
+    hobbiesDesc[2].classList.add("home-text");
+};
 
-function nextIcon2() {
-   hobbies[2].classList.add("home-icons3");
-   hobbiesDesc[2].classList.add("home-text3");
-}
+var blah4 = function test (){
+    hobbies[3].classList.add("home-icons");
+    hobbiesDesc[3].classList.add("home-text");
+};
 
-function nextIcon3() {
-   hobbies[3].classList.add("home-icons4");
-   hobbiesDesc[3].classList.add("home-text4");
-}
+var blah5 = function test (){
+    hobbies[4].classList.add("home-icons");
+    hobbiesDesc[4].classList.add("home-text");
+};
 
-function nextIcon4() {
-   hobbies[4].classList.add("home-icons5");
-   hobbiesDesc[4].classList.add("home-text5");
-}
-
-function nextIcon5() {
-   hobbies[5].classList.add("home-icons6");
-   hobbiesDesc[5].classList.add("home-text6");
-}
+var blah6 = function test (){
+    hobbies[5].classList.add("home-icons");
+    hobbiesDesc[5].classList.add("home-text");
+};
 
 
-
-hobbies[0].addEventListener("transitionend", nextIcon, false);
-hobbies[1].addEventListener("transitionend", nextIcon2, false);
-hobbies[2].addEventListener("transitionend", nextIcon3, false);
-hobbies[3].addEventListener("transitionend", nextIcon4, false);
-hobbies[4].addEventListener("transitionend", nextIcon5, false);
-
-/* jshint ignore: end */
+setTimeout(blah1,500);
+setTimeout(blah2,750);
+setTimeout(blah3,1000);
+setTimeout(blah4,1250);
+setTimeout(blah5,1500);
+setTimeout(blah6,1750);
+            
