df2b624c46903b63cd3b82e6cec9b94ffca1d97c
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-10-01 02:04:11 -0500

    updating animations

--- src/js/home.js
+++ src/js/home.js
@@ -1,28 +1,36 @@
 var hobbies = document.getElementsByClassName("hobbies");
-var active = false;
+var hobbiesDesc = document.getElementsByClassName("hobbies-desc");
 
-hobbies[0].onclick = function() {
-   hobbies[0].classList.add("test");
-};
+for (i = 0; i < 6; i++) {
+    hobbies[i].onclick = function() {
+        hobbies[0].classList.add("test");
+        hobbiesDesc[0].classList.add("blah");
+    };
+}
 
 function nextIcon() {
    hobbies[1].classList.add("test2");
+   hobbiesDesc[1].classList.add("blah2");
 }
 
 function nextIcon2() {
    hobbies[2].classList.add("test3");
+   hobbiesDesc[2].classList.add("blah3");
 }
 
 function nextIcon3() {
    hobbies[3].classList.add("test4");
+   hobbiesDesc[3].classList.add("blah4");
 }
 
 function nextIcon4() {
    hobbies[4].classList.add("test5");
+   hobbiesDesc[4].classList.add("blah5");
 }
 
 function nextIcon5() {
    hobbies[5].classList.add("test6");
+   hobbiesDesc[5].classList.add("blah6");
 }
 
 
